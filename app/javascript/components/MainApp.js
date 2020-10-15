a9b8f9f7c303834ea0ab77920c456a87032b3e09
Diego Plancarte <dplancarte7@gmail.com>
2020-10-14 19:02:33 -0700

    Created React in Rails app for testing. installed React and Devise.

---
+++ app/javascript/components/MainApp.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class MainApp extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Hello World!!</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MainApp
