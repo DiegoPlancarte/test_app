2b6bd9e698b8bdec6e90d05319118682afafc670
Diego Plancarte <dplancarte7@gmail.com>
2020-10-14 21:20:00 -0700

    added bootstrap, bootstrap theme, reactstrap, react-router and react-router-dom

--- app/javascript/components/MainApp.js
+++ app/javascript/components/MainApp.js
@@ -1,10 +1,13 @@
 import React from "react"
+import { Jumbotron } from 'reactstrap'
 
 class MainApp extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <h1>Hello World!!</h1>
+        <Jumbotron>
+          <h1>Hello World!!</h1>
+        </Jumbotron>
       </React.Fragment>
     );
   }
