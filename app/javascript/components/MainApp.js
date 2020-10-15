97135cb6d846b26ccea73f77b95ab1d3585d94d1
Diego Plancarte <dplancarte7@gmail.com>
2020-10-15 10:07:55 -0700

    created navbar. used react hooks for toggle.

--- app/javascript/components/MainApp.js
+++ app/javascript/components/MainApp.js
@@ -1,13 +1,19 @@
 import React from "react"
-import { Jumbotron } from 'reactstrap'
+import { Jumbotron, Button } from 'reactstrap'
+import Navigation from "./pages/Navigation"
 
 class MainApp extends React.Component {
   render () {
+    const {
+			signed_in,
+			sign_in_route,
+			sign_out_route,
+			current_user,
+			new_user_registration_path
+		} = this.props
     return (
       <React.Fragment>
-        <Jumbotron>
-          <h1>Hello World!!</h1>
-        </Jumbotron>
+        <Navigation signed_in={signed_in} sign_in_route={sign_in_route} sign_out_route={sign_out_route} new_user_registration_path={new_user_registration_path}/>
       </React.Fragment>
     );
   }
